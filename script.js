5df01e486ad3498223bb6766c3e0b90508478d54
trentonwagner-ai <trenton.wagner@stu.barren.kyschools.us>
2026-03-03 11:00:50 -0600

    Not finished Text Adventure

--- script.js
+++ script.js
@@ -25,11 +25,11 @@ if (race == "Orc"){
         health -= 5;
         magicka += 25;
 } else {
-    race = "Nord"
+    race = "Nord"}
+// Border choice
+    let border = prompt("You have been thinking about leaving Skyrim for a long time. You come up upon a bridge that spans into the next province. Do you A. Continue across the border or B. Turn around and go home.");
+if (border == "B") {alert("You turn around and go home staying in Skyrim. After years of hard work in Skyrim you retire.")}
+else if (border == "A") {encounter = prompt("Just your luck, you have crossed at the same time where Imperial and Stormcloak forces meet. The Imperials believe you are with the Stormcloaks. So, what do you do? A. Fight along with the Stormcloaks. B. Give up and lay down your arms. ")}
 
-}
-
-
-
-
-
+if (encounter == "A"){combat = prompt ("Time to fight for your life! A light Imperial infantry rushes at you with a sword and shield. What you decide will lead to life or death. Do you A. Cast a fire bolt and fry the poor soldier. (-25 magicka) B. Block the soldier's attack (-15 stamina) C. Swing at the soldier with your weapon (-25 stamina)")}
+else if (encounter == "B") {alert ("You are captured by the Imperials! You are taken with the Stormcloak rebels to Helgen to be executed.")}
